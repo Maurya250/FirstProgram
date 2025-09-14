e001d6603d32d58e2b47f576ea32648970f09b9a
Maurya250 <aniketmaurya.dev@gmail.com>
2025-09-14 14:23:30 +0530

    Change to code

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("i am a DevOps Engineer");
     }
     
 }
